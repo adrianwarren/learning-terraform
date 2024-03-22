3c5df7f33c8248fa87bfe7e37475b1fcfd1bf291
Adrian Warren <adrian@warren-usa.com>
2024-03-22 07:55:27 -0700

    reorg

--- main.tf
+++ main.tf
@@ -69,18 +69,18 @@ module "alb" {
 
       }
     }
+    http_tcp_listeners = [
+      {
+        port               = 80
+        protocol           = "HTTP"
+        target_group_index = 0
+      }
+    ]
+    tags = {
+      Environment = "dev"
+    }
   }
 
-  http_tcp_listeners = {
-  {
-    port               = 80
-    protocol           = "HTTP"
-    target_group_index = 0
-  }
-
-tags = {
-  Environment = "dev"
-}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
