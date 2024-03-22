f5e3ad2ab54958bc0bde9e1c01ebe7dc582aa7ac
Adrian Warren <adrian@warren-usa.com>
2024-03-22 08:01:34 -0700

    mo cleanup

--- main.tf
+++ main.tf
@@ -55,32 +55,33 @@ module "alb" {
 
 
 
-  target_groups = {
-    ex-instance = {
-      name_prefix = "blog-"
-      protocol    = "HTTP"
-      port        = 80
-      target_type = "instance"
-      target      = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
+  target_groups = [
+    {
+      ex-instance = {
+        name_prefix = "blog-"
+        protocol    = "HTTP"
+        port        = 80
+        target_type = "instance"
+        target      = {
+          my_target = {
+            target_id = aws_instance.blog.id
+            port      = 80
+          }
 
+        }
       }
-    }
-    http_tcp_listeners = [
-      {
-        port               = 80
-        protocol           = "HTTP"
-        target_group_index = 0
+      http_tcp_listeners = [
+        {
+          port               = 80
+          protocol           = "HTTP"
+          target_group_index = 0
+        }
+      ]
+      tags = {
+        Environment = "dev"
       }
-    ]
-    tags = {
-      Environment = "dev"
-    }
   }
-}
+]
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
