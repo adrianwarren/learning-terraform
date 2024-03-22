30b0bf270690ddaa65d1fcccb833306be9959175
Adrian Warren <adrian@warren-usa.com>
2024-03-22 08:35:20 -0700

    again??

--- main.tf
+++ main.tf
@@ -69,14 +69,17 @@ module "alb" {
       }
     }
   ]
+  
+  listeners = {
+    http_tcp_listeners = [
+      {
+        port               = 80
+        protocol           = "HTTP"
+        target_group_index = 0
+      }
+    ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+  }
 
   tags = {
     Environment = "dev"
